bfc2239c989dd25733b53f267cb18bfd314411a4
Julia <julia@datarac.es>
2020-04-08 11:36:30 +0100

    add a 3-participant dining philosophers variant. Runs within 2mn on my old mbp, should run within minutes on most machines

---
+++ dine3-chan-fix/main.go
@@ -0,0 +1,55 @@
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func Fork(fork *int, ch chan int) {
+	for {
+		*fork = 1
+		ch <- 0
+		<-ch
+	}
+}
+
+func phil(fork1, fork2 *int, ch1, ch2 chan int, id int) {
+	for {
+		select {
+		case <-ch1:
+			select {
+			case <-ch2:
+				fmt.Printf("phil %d got both fork\n", id)
+				ch1 <- *fork1
+				ch2 <- *fork2
+			default:
+				ch1 <- *fork1
+			}
+		case <-ch2:
+			select {
+			case <-ch1:
+				fmt.Printf("phil %d got both fork\n", id)
+				ch2 <- *fork2
+				ch1 <- *fork1
+			default:
+				ch2 <- *fork2
+			}
+		}
+	}
+}
+
+func main() {
+	var fork1, fork2, fork3 int
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan int)
+	go phil(&fork1, &fork2, ch1, ch2, 0)
+	go phil(&fork2, &fork3, ch2, ch3, 1)
+	go phil(&fork3, &fork1, ch3, ch1, 2)
+	go Fork(&fork1, ch1)
+	go Fork(&fork2, ch2)
+	go Fork(&fork3, ch3)
+	time.Sleep(10*time.Second)
+}
